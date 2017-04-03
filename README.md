afdf3b883e505a65e9132a186f9c0f1f1557d49c
paulgoblin <mpr75@cornell.edu>
2017-04-03 00:36:17 -0700

    First

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Performance Tests
+
+For testing performance of basic JS operations on your machine. *Chrome only*
+
+[Try it out!](http://paulgoblin.github.io/perf-test)
+
---
+++ main.js
@@ -0,0 +1,88 @@
+'use strict';
+
+(function() {
+  // settings
+  const n = 100000;
+
+  // utils
+  const displayPerf = (fn, node) => (...args) => {
+    const start = performance.now();
+    const result = fn(...args);
+    const end = performance.now();
+    const time = (end - start).toString().slice(0, 5);
+    node.innerHTML = time;
+    return result;
+  }
+
+
+  // sum with for loop
+  const loopSumCount = document.getElementById('loopSumCount');
+  const startLoopSumButton = document.getElementById('startLoopSumButton');
+  const loopSumResult = document.getElementById('loopSumResult');
+  loopSumCount.innerHTML = n;
+
+  const loopSumNCounts = n => {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += i
+    }
+    return sum;
+  }
+
+  startLoopSumButton.addEventListener('click',
+    displayPerf(() => loopSumNCounts(n), loopSumResult)
+  );
+
+
+  // sum with forEach
+  const sumCount = document.getElementById('sumCount');
+  const startSumButton = document.getElementById('startSumButton');
+  const sumResult = document.getElementById('sumResult');
+  sumCount.innerHTML = n;
+
+  const sumNCounts = n => {
+    return Array.from({ length: n })
+      .reduce((sum, i) => sum + i, 0);
+  }
+
+  startSumButton.addEventListener('click',
+    displayPerf(() => sumNCounts(n), sumResult)
+  );
+
+
+  // render
+  const renderCount = document.getElementById('renderCount');
+  const countDisplay = document.getElementById('countDisplay');
+  const startCountButton = document.getElementById('startCountButton');
+  const countResult = document.getElementById('countResult');
+  countDisplay.innerHTML = '-';
+  renderCount.innerHTML = n;
+
+  const renderNCounts = n => {
+    for (let i = 1; i <= n; i++) {
+      countDisplay.innerHTML = i;
+    }
+  }
+
+  startCountButton.addEventListener('click',
+    displayPerf(() => renderNCounts(n), countResult)
+  );
+
+
+  // call API
+  const callApiButton = document.getElementById('callApiButton');
+  const callApiResult = document.getElementById('callApiResult');
+
+  const callApi = cb => {
+    const start = performance.now();
+    return fetch('http://swapi.co/api/people/1', {
+      method: 'head'
+    }).then(() => {
+      const end = performance.now();
+      const time = (end - start).toString().slice(0, 5);
+      callApiResult.innerHTML = time;
+    })
+  }
+
+  callApiButton.addEventListener('click', callApi);
+})();
